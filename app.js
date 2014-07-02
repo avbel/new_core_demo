c0f982322337d0bd17796c8b0643890fb1e4dd49
Jeff Jagoda <jjagoda@bandwidth.com>
2014-07-02 13:21:50 -0400

    Make everything a plugin.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 "use strict";
-let co = require("co");
 let core = require("./lib"); // will be require("labs_core"); in real project
 
-co(core())(function(err){
-  if(err){
-    console.error(err.stack);
+core(
+  function* (labs) {
+    let app = yield labs.dependency("app");
+
+    app.get("/custom", function* () {
+      this.body = "custom endpoint";
+    });
+  },
+  function (error) {
+    if(error){
+      console.error(error.stack);
+    }
   }
-});
+);
