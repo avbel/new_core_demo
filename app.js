fb812f5cc14227e5a22389353bb41f70d7e74b02
Andrey Belchikov <belchikovs@gmail.com>
2014-07-02 04:37:51 -0700

    Added moree comments

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 let co = require("co");
-let core = require("./lib");
+let core = require("./lib"); // will be require("labs_core"); in real project
 
 co(core())(function(err){
   if(err){
